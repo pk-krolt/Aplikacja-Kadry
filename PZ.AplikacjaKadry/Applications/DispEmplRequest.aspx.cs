ad0e5dcdc38250c23ee705543a4bf26d406c22d5
Tomasz Król <37451573+pk-krolt@users.noreply.github.com>
2018-06-14 23:42:11 +0200

    Implementacja modulu wniosków o zatrudnienie
    Implementacja modulu wniosków o zatrudnienie #39
    
    Co-Authored-By: l3vvi <l3vvi@users.noreply.github.com>

---
+++ PZ.AplikacjaKadry/Applications/DispEmplRequest.aspx.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SZRBD.Kadry.Applications
+{
+    public partial class DispEmplRequest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public Models.EmploymentRequest GetItem([QueryString("ID")] int? Id)
+        {
+            Models.EmploymentRequest item = new Models.EmploymentRequest();
+
+            if (Id != null)
+            {
+                var constr = System.Configuration.ConfigurationManager.ConnectionStrings["KadryConnection"].ConnectionString;
+                SqlConnection con = new SqlConnection(constr);
+                SqlDataAdapter dataAdapter = new SqlDataAdapter("select * from dbo.WNIOSEK_ZATRUDNIENIE inner join dbo.OGLOSZENIE_STANOWISKO on dbo.WNIOSEK_ZATRUDNIENIE.Id_ogloszenie = dbo.OGLOSZENIE_STANOWISKO.Id_ogloszenie where Id_aplikacja = @Id_aplikacja", con);
+                dataAdapter.SelectCommand.Parameters.AddWithValue("@Id_aplikacja", Id);
+                DataSet ds = new DataSet();
+                dataAdapter.Fill(ds, "WNIOSEK_ZATRUDNIENIE");
+
+                if (ds.Tables["WNIOSEK_ZATRUDNIENIE"].Rows.Count > 0)
+                {
+                    var row = ds.Tables["WNIOSEK_ZATRUDNIENIE"].Rows[0];
+
+                    item.AdvertName = row["Nazwa_ogloszenie"].ToString();
+                    item.AdvertLink = "/Job/Advert.aspx?ID=" + row["Id_ogloszenie"].ToString();
+                    item.Name = row["Imie_aplikant"].ToString();
+                    item.Surname = row["Nazwisko_aplikant"].ToString();
+                    item.Date = row["Data_wniosku"].ToString();
+                    item.Phone = row["Telefon"].ToString();
+                    item.Email = row["Email"].ToString();
+                    item.Status = row["Status"].ToString();
+
+                    if(!String.IsNullOrEmpty(row["Feedback"].ToString()))
+                    {
+                        YourAnswerBox.Text = row["Feedback"].ToString();
+                        YourAnswerBox.Enabled = false;
+                    }
+
+                    if(row["Status"].ToString() != "Oczekuje")
+                    {
+                        Accept.Visible = false;
+                        Decline.Visible = false;
+                    }
+                    
+                }
+            }
+            
+            return item;
+        }
+
+        protected void Accept_Click(object sender, EventArgs e)
+        {
+            var id = Request.QueryString["ID"];
+
+            if (id != null)
+            {
+                var constr = System.Configuration.ConfigurationManager.ConnectionStrings["KadryConnection"].ConnectionString;
+                SqlConnection con = new SqlConnection(constr);
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand(@"update dbo.WNIOSEK_ZATRUDNIENIE set Status = @Status,
+                                                                                      Feedback = @Feedback
+                                                                                      where Id_aplikacja = @Id", con);
+                cmd.Parameters.AddWithValue("@Id", id);
+                cmd.Parameters.AddWithValue("@Status", "Zaakceptowano");
+                cmd.Parameters.AddWithValue("@Feedback", YourAnswerBox.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                //Send email
+                if (YourAnswerBox.Text != null)
+                {
+
+                }
+
+                Response.Redirect("Employment.aspx");
+            }
+        }
+
+        protected void Decline_Click(object sender, EventArgs e)
+        {
+            var id = Request.QueryString["ID"];
+
+            if(id != null)
+            {
+                var constr = System.Configuration.ConfigurationManager.ConnectionStrings["KadryConnection"].ConnectionString;
+                SqlConnection con = new SqlConnection(constr);
+                con.Open();
+                SqlCommand cmd = new SqlCommand(@"update dbo.WNIOSEK_ZATRUDNIENIE  set Status = @Status
+                                                                                   where Id_aplikacja = @Id", con);
+                cmd.Parameters.AddWithValue("@Id", id);
+                cmd.Parameters.AddWithValue("@Status", "Odrzucony");
+                cmd.ExecuteNonQuery();
+
+                Response.Redirect("Employment.aspx");
+            }
+        }
+    }
+}
---
+++ PZ.AplikacjaKadry/Applications/Holidays.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SZRBD.Kadry;
+
+namespace SZRBD.Kadry.Applications
+{
+    public partial class Holidays : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string login = HttpContext.Current.User.Identity. Name;
+            string id_pracownik = Helper.GetEmployeeIdByEmail(login);
+
+            if (id_pracownik != null)
+            {
+                bool isManager = HttpContext.Current.User.IsInRole(Consts.Roles.Administrator) || HttpContext.Current.User.IsInRole(Consts.Roles.Manager);
+
+                var constr = System.Configuration.ConfigurationManager.ConnectionStrings["KadryConnection"].ConnectionString;
+                SqlConnection con = new SqlConnection(constr);
+
+                if (isManager)
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(@"select * from dbo.WNIOSEK_URLOP
+                                                                  inner join dbo.PRACOWNIK on dbo.WNIOSEK_URLOP.Id_pracownik = dbo.PRACOWNIK.Id_pracownik
+                                                                  where Status_urlop = 'Oczekuje'", con);
+                    DataSet ds = new DataSet();
+                    adapter.Fill(ds, "WNIOSEK_URLOP");
+
+                    DataView dv1 = ds.Tables["WNIOSEK_URLOP"].AsDataView();
+                    dv1.RowFilter = "Id_pracownik = '" + id_pracownik + "'";
+                    MyHolidaysList.DataSource = dv1;
+                    MyHolidaysList.DataBind();
+
+                    DataView dv2 = ds.Tables["WNIOSEK_URLOP"].AsDataView();
+                    dv2.RowFilter = "Id_pracownik <> '" + id_pracownik + "'";
+                    EmployeesHolidaysList.DataSource = dv2;
+                    EmployeesHolidaysList.DataBind();
+                }
+                else
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(@"select * from dbo.WNIOSEK_URLOP
+                                                                  inner join dbo.PRACOWNIK on dbo.WNIOSEK_URLOP.Id_pracownik = dbo.PRACOWNIK.Id_pracownik
+                                                                  where dbo.WNIOSEK_URLOP.Id_pracownik = @Id_pracownik", con);
+                    adapter.SelectCommand.Parameters.AddWithValue("@Id_pracownik", id_pracownik);
+
+                    DataSet ds = new DataSet();
+                    adapter.Fill(ds, "WNIOSEK_URLOP");
+                    MyHolidaysList.DataSource = ds.Tables["WNIOSEK_URLOP"];
+                    MyHolidaysList.DataBind();
+                }
+            }
+        }
+    }
+}
