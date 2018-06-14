a786caf0c28e06a2d8692edf12cd660f87962b41
Tomasz Król <37451573+pk-krolt@users.noreply.github.com>
2018-06-14 22:59:48 +0200

    Dodanie klas modeli dla ogłoszeń o pracę
    Co-Authored-By: piotr9955 <piotr9955@users.noreply.github.com>
    
    #47

---
+++ PZ.AplikacjaKadry/Models/Position.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SZRBD.Kadry.Models
+{
+    public class Position
+    {
+        public string PositionID;
+        public string Name;
+        public string Department;
+    }
+}
